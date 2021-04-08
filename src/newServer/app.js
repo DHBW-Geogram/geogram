c136d9d2cde2330066d8f50c09c6cc1f461dcc12
pafinkbeiner <paul-d-f@web.de>
2021-04-08 15:21:52 +0200

    asdasdasd

--- src/newServer/app.js
+++ src/newServer/app.js
@@ -1,82 +1,58 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const { v4: uuidv4 } = require('uuid');
-const cors = require("cors")
-const {writeFileSync} = require('fs')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+const { writeFileSync } = require("fs");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false , limit: '50mb'}));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, "public")));
 
-const allowedOrigins = [
-  'capacitor://localhost',
-  'ionic://localhost',
-  'http://localhost',
-  'http://localhost:8080',
-  'http://localhost:8100',
-  '*'
-];
+app.use(cors())
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Origin not allowed by CORS'));
-    }
+app.post("/upload1", (req, res) => {
+  console.log("Request on res /upload1");
+
+  // Create new Filename
+  let filename = `${uuidv4()}.jpg`;
+
+  console.log(`Created File with name: ${filename}`);
+
+  // Export file from body request
+  let image = req.body.data;
+
+  console.log(`Received Image: `);
+
+  // Set up variables for error handling
+  let buffer;
+  let e = undefined;
+
+  // Try to write file to webserver
+  //   try{
+
+  buffer = Buffer.from(image, "base64");
+  writeFileSync(`public/uploads/${filename}`, buffer);
+
+  //   }catch(err){
+  //     if(err) e = err;
+  //   }
+
+  // Give App Feedback
+  if (e) {
+    console.log(`Saving image failed:!!`);
+    res.json({ file: undefined });
+  } else {
+    console.log(`Saved image at: /uploads/${filename}`);
+    res.json({
+      file: `uploads/${filename}`,
+    });
   }
-}
-
-app.options('*', cors(corsOptions));
-
-// app.use(cors())
-
-app.post('/upload1',(req, res) => {
-
-    console.log("Request on res /upload1")
-  
-      // Create new Filename
-      let filename = `${uuidv4()}.jpg`;
-  
-      console.log(`Created File with name: ${filename}`)
-  
-      // Export file from body request
-      let image = req.body.data;
-  
-      console.log(`Received Image: `)
-  
-      // Set up variables for error handling
-      let buffer; 
-      let e = undefined
-  
-      // Try to write file to webserver
-    //   try{
-  
-        buffer = Buffer.from(image, 'base64')
-        writeFileSync(`public/uploads/${filename}`, buffer)
-  
-    //   }catch(err){
-    //     if(err) e = err;
-    //   }
-  
-      // Give App Feedback
-      if(e){
-        console.log(`Saving image failed:!!`)
-        res.json({file: undefined})
-      }else{
-        console.log(`Saved image at: /uploads/${filename}`)
-        res.json({
-          file: `uploads/${filename}`
-        });
-      }
-  
-  });
+});
 
 module.exports = app;
