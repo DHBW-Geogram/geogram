58d03f1219c6895715323814df0263a28cb56450
JosuaStricker <josua.stricker@t-online.de>
2021-04-23 08:35:40 +0200

    add check auth email with user collection emil

---
+++ src/client/src/hooks/checkAuthEmailWithUserCollectionEmail.ts
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { UserContext } from "..";
+import { auth, db } from "../helper/firebase";
+
+import firebase from "firebase/app";
+
+
+export async function checkAuthEmailWithUserCollectionEmail(user: firebase.User | null) {
+    
+  db.collection("users")
+    .doc(user?.uid)
+    .get()
+    .then(async (documentSnapshot) => {
+      if (documentSnapshot.data()?.email === auth.currentUser?.email) {
+        return;
+      } else {
+        db.collection("users").doc(user?.uid).update({
+          email: auth.currentUser?.email,
+        });
+      }
+    });
+}
