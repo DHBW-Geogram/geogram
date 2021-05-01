f3bd5ebf0578b61efa0d9c6b29288d018d935737
JosuaStricker <josua.stricker@t-online.de>
2021-05-01 11:45:27 +0200

    add

--- src/client/src/helper/firebase.ts
+++ src/client/src/helper/firebase.ts
@@ -1,12 +1,20 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-      apiKey: "AIzaSyCuOUn1kd_QKs91UYx2Pv015QYAwNfpkWg",
-      authDomain: "geogram-4906f.firebaseapp.com",
-      projectId: "geogram-4906f",
-      storageBucket: "geogram-4906f.appspot.com",
-      messagingSenderId: "293875577588",
-      appId: "1:293875577588:web:d286bacade6e4850bfa4a1"
+    apiKey: "AIzaSyBXSt86U-kxZjHd6K6n_Ja13z0naCXnw0w",
+    authDomain: "geogram-backup.firebaseapp.com",
+    projectId: "geogram-backup",
+    storageBucket: "geogram-backup.appspot.com",
+    messagingSenderId: "308731272904",
+    appId: "1:308731272904:web:4503db99c094a163eccffd",
+    measurementId: "G-DYTVK15115"
+
+    //   apiKey: "AIzaSyCuOUn1kd_QKs91UYx2Pv015QYAwNfpkWg",
+    //   authDomain: "geogram-4906f.firebaseapp.com",
+    //   projectId: "geogram-4906f",
+    //   storageBucket: "geogram-4906f.appspot.com",
+    //   messagingSenderId: "293875577588",
+    //   appId: "1:293875577588:web:d286bacade6e4850bfa4a1"
  };
 
 // Configure
